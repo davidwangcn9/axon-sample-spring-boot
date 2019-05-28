db3ec4116448d292f889836ac0b1fee5e729b96a
yichen.wang <yichen.wang@thoughtworks.com>
2019-05-28 21:49:52 +0800

    add step4 feature

--- src/main/java/com/ote/test/axonsample/command/Feature.kt
+++ src/main/java/com/ote/test/axonsample/command/Feature.kt
@@ -3,3 +3,4 @@ package com.ote.test.axonsample.command
 class Feature(var name:String, var age:Int)
 class Step_2_on_FeatureA(val value:Double)
 class Step_3_on_FeatureA(val value:Double)
+class Step_4_on_FeatureA(val value:Double)
